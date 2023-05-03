67e1f933674ce928654bb8fd20e190112507c349
GitDidrik <lindberg.didrik@gmail.com>
2023-05-03 11:22:20 -0700

    server and connection

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// require sequelize
+const sequelize = require('../config/connection');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW, {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
+
+// export the connection
+module.exports = sequelize;
